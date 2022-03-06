3d0efe460f1fc499f29c37c8a0084fa3922abf6c
krismon setiawan <krismonsetiawan57@gmail.com>
2022-03-07 04:04:27 +0700

    Related Post Fetching

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services'
 
-const PostWidget = () => {
-  return <div>Post Widget</div>
+type PostWidgetProps = {
+  categories: string
+  slug: string
+}
+
+// type PostWidgetState = {
+//   relatedPosts: []
+// }
+
+// interface RelatedPostProps {
+//   relatedPosts: []
+// }
+
+const PostWidget = ({ categories, slug }: PostWidgetProps) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      )
+    } else {
+      getRecentPosts(categories, slug).then((result) => setRelatedPosts(result))
+    }
+  }, [slug])
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        {slug ? 'Related Post' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((relatedPost) => (
+        <div className="mb-4 flex w-full items-center" key={relatedPost.slug}>
+          <div className="w-16 flex-none">
+            <img
+              src={relatedPost.featuredImage.url}
+              alt={relatedPost.title}
+              width="60px"
+              height="60px"
+              className="rounded-full align-middle"
+            />
+          </div>
+          <div className="ml-4 flex-grow">
+            <p className="text-grey-500 font-xs">
+              {moment(relatedPost.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link href={`/post/${relatedPost.slug}`} key={relatedPost.slug}>
+              <span className="text-md">{relatedPost.title}</span>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default PostWidget
--- services/index.js
+++ services/index.js
@@ -38,3 +38,42 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges
 }
+
+// fetch recent posts
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(orderBy: createdAt_DESC, last: 3) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query)
+
+  return result.posts
+}
+
+export const getSimilarPosts = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $catogries: [String!]) {
+      posts(where: { slug_not: $slug, AND { categories_some: { slug_in: $slug } } }, last: 3) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.posts
+}
