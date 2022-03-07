ac1399bfca738538c75a9824a167848983ebcac3
krismon setiawan <krismonsetiawan57@gmail.com>
2022-03-07 21:36:09 +0700

    fix error category.tsx

--- components/Category.tsx
+++ components/Category.tsx
@@ -4,6 +4,13 @@ import Link from 'next/link'
 // services
 import { getCategories } from '../services'
 
+type categoryType = {
+  category: {
+    name: string
+    slug: string
+  }
+}
+
 const Category = () => {
   const [categories, setCategroies] = useState([])
 
@@ -15,7 +22,7 @@ const Category = () => {
     <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
       <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
 
-      {categories.map((category) => (
+      {categories.map(({ category }: categoryType) => (
         <Link href={`/category/${category.slug}`} key={category.slug}>
           <span className="mb-3 block cursor-pointer pb-3">
             {category.name}
